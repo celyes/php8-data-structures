084e87b1b16fa6d1968f52540990599a4be988cb
celyes <celyes02@gmail.com>
2019-02-03 00:36:36 +0100

    create files - src/arrays/numeric_array.php, readme.md, .gitignore

---
+++ src/arrays/numeric_array.php
@@ -0,0 +1,9 @@
+<?php 
+
+// Creating an array and looping through its values
+
+$numeric_array = array(1, 2, 3, 4, 5);
+
+foreach($numeric_array as $element){
+    echo "the value is : {$element} <br>";
+}
