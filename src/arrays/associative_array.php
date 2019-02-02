1b31aa5f8542ce0aebabf62ffe384d259b6d9868
celyes <celyes02@gmail.com>
2019-02-03 00:42:44 +0100

    create file - src/arrays/associative_array.php

---
+++ src/arrays/associative_array.php
@@ -0,0 +1,9 @@
+<?php 
+/* 
+    associative array can have indexes that are 
+    not numerical unlike the numeric arrays 
+*/
+$associative_array = array("name"=>"John", "age"=>25, "job"=>"coder");
+foreach($associative_array as $key=>$value){
+    echo "{$key} : {$value} <br>";
+}
