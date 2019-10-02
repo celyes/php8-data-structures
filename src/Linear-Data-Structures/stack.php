7e626fe325e3061af6f060f1da6930752b1c7686
celyes <celyes02@gmail.com>
2019-10-02 17:18:48 +0100

    update and improve stack class

--- src/Linear-Data-Structures/stack.php
+++ src/Linear-Data-Structures/stack.php
@@ -7,12 +7,33 @@ class Stack implements StackInterface{
     protected $stack;
     protected $limit;
 
-    public function __construct(array $initiate = array(), int $limit = 10){
-        $this->stack = $initiate;
+    /**
+     * __construct: sets some initial variables
+     *
+     * @param  mixed $initiate sets the property stack to a given array.
+     * @param  mixed $limit sets the maximum length of the stack..
+     *
+     * @return void
+     */
+    public function __construct($initiate, $limit = 10)
+    {
+        if(empty($initiate)){
+            $initiate = [];
+        }
+        $this->stack = array_reverse($initiate);
         $this->limit = $limit;
     }
    
-    public function push($item){
+    /**
+     * push : adds a new item to the end of the stack
+     *
+     * @param  mixed $item
+     *
+     * @return void 
+     */
+    public function push($item)
+    
+    {
         
         if(count($this->stack) < $this->limit){
             array_unshift($this->stack , $item);
@@ -20,20 +41,54 @@ class Stack implements StackInterface{
             throw new RunTimeException("Stack is full!");
         }
     }
-    public function pop(){
+
+    
+    /**
+     * pop : removes the last item of the stack
+     *
+     * @return mixed 
+     */
+    public function pop()
+    {
         if($this->isEmpty()){
-            throw new RunTimeException("Stack is empty!");
+            throw new \RunTimeException("Stack is empty!");
         }else{
             return array_shift($this->stack);
         }
     }
-    public function top(){
+    
+
+    /**
+     * top : returns the content of the last element added to the stack
+     *
+     * @return mixed
+     */
+
+    public function top()
+    {
         return $this->stack[0];
     }
-    public function all(){
+
+
+    /**
+     * all : returns all the items in the stack as an array
+     *
+     * @return array
+     */
+
+    public function all()
+    {
         return $this->stack;
     }
-    public function isEmpty(){
+
+    
+    /**
+     * isEmpty : checks if the stack is empty.
+     *
+     * @return boolean
+     */
+    public function isEmpty()
+    {
         return empty($this->stack);
     }
 }
