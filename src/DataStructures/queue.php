ae9202f952386b96b6388c724ecd39222e1295fc
celyes <celyes02@gmail.com>
2022-06-05 19:30:59 +0100

    add queues

--- src/DataStructures/queue.php
+++ src/DataStructures/queue.php
@@ -1,9 +1,34 @@
 <?php
 
 namespace Celyes\DataStructures;
-
-require __DIR__.'\..\..\vendor\autoload.php';
-
 final class Queue
 {
+    protected $pool = [];
+
+    /**
+     * enqueue - Adds an element to the queue
+     * @param mixed $element - The element to be added to the queue
+     */
+    public function enqueue($element)
+    {
+        $this->pool[] = $element;
+    }
+
+    /**
+     * dequeue - shifts an element from the queue
+     * @return mixed - the shifted element
+     */
+    public function dequeue()
+    {
+        return array_shift($this->pool);
+    }
+
+    /**
+     * count - gets the count of element in the queue
+     * @return int - the count of how many elements are in the queue
+     */
+    public function count()
+    {
+        return count($this->pool);
+    }
 } 
--- tests/DataStructures/QueueTest.php
+++ tests/DataStructures/QueueTest.php
@@ -7,8 +7,31 @@ use Celyes\DataStructures\Queue;
 
 final class QueueTest extends TestCase
 {
-    public function testEnqueueWorksOnQueue()
+    protected $queue;
+    public function setup(): void
     {
-        
+        $this->queue = new Queue();
+    }
+
+    /**
+     * @test
+     */
+    public function isEnqueueWorking()
+    {
+        $this->queue->enqueue(1);
+        $this->queue->enqueue(2);
+        $this->assertEquals(2, $this->queue->count());
+    }
+
+    /**
+     * @test
+     */
+    public function isDequeueWorking()
+    {
+        $this->queue->enqueue(1);
+        $this->queue->enqueue(2);
+        $this->queue->enqueue(3);
+        $this->queue->dequeue(2); // we've [1, 3] left
+        $this->assertEquals(2, $this->queue->count());
     }
 }
